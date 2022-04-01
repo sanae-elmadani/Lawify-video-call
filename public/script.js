670f7281d232c3a11b296be39b482de99eda2761
sanae-elmadani <sanaeelmadani3@gmail.com>
2022-04-01 17:15:43 +0200

    first commit

--- public/script.js
+++ public/script.js
@@ -24,10 +24,14 @@ const user = prompt("Enter your name");
 var peer = new Peer(undefined, {
   path: "/peerjs",
   host: "/",
-  port: "443",
+  port: "3030",
 });
 
 let myVideoStream;
+var getUserMedia =
+  navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia;
 navigator.mediaDevices
   .getUserMedia({
     audio: true,
@@ -47,20 +51,43 @@ navigator.mediaDevices
 
     socket.on("user-connected", (userId) => {
       connectToNewUser(userId, stream);
-    });
+    }); 
+  });
+  peer.on("call", function (call) {
+    getUserMedia(
+      { video: true, audio: true },
+      function (stream) {
+        call.answer(stream); // Answer the call with an A/V stream.
+        const video = document.createElement("video");
+        call.on("stream", function (remoteStream) {
+          addVideoStream(video, remoteStream);
+        });
+      },
+      function (err) {
+        console.log("Failed to get local stream", err);
+      }
+    );
   });
 
-const connectToNewUser = (userId, stream) => {
-  const call = peer.call(userId, stream);
-  const video = document.createElement("video");
-  call.on("stream", (userVideoStream) => {
-    addVideoStream(video, userVideoStream);
-  });
-};
+// const connectToNewUser = (userId, stream) => {
+//   const call = peer.call(userId, stream);
+//   const video = document.createElement("video");
+//   call.on("stream", (userVideoStream) => {
+//     addVideoStream(video, userVideoStream);
+//   });
+// };
 
 peer.on("open", (id) => {
   socket.emit("join-room", ROOM_ID, id, user);
 });
+
+const connectToNewUser = (userId, stream) => {
+const call = peer.call(userId, stream);
+const video = document.createElement("video");
+call.on("stream", (userVideoStream) => {
+ addVideoStream(video, userVideoStream);
+});
+};
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
